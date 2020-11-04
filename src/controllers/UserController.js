1cfeb334dcdf6c242b709441cfe1193e62b1bde8
Wurillo Welsi <murillo.welsi@gmail.com>
2020-11-04 13:48:35 +0000

    Correção bug password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,10 +2,10 @@ import User from '../models/User'
 
 class UserCOntroller {
   async store(req, res) {
-    const {name, email, pasword} = req.body;
+    const {name, email, password} = req.body;
 
     const user = await User.create({
-      name, email, pasword
+      name, email, password
     });
 
     return res.json({user:user.show()})
